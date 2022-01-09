692a12482a536ca2045a980eef00c138f8d558e2
lonsdale228 <gerashchenkovladislav@onua.edu.ua>
2022-01-09 23:48:41 +0200

    Добавьте файлы проекта.

---
+++ metods/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metods
+{
+    class Person
+    {
+        public string SecondName { get; set; }
+        public string Name { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Person(string secondName, string name)
+        {
+            SecondName = secondName;
+            Name = name;
+
+            X = 0;
+            Y = 0;
+
+        }
+
+        public string Run(int x)
+        {
+            var rnd = new Random();
+
+            X += rnd.Next(-2,2);
+            Y += rnd.Next(-2,2);
+
+            return $"{Name} ({X},{Y})";
+        }
+
+        public string Run(Person y)
+        {
+            var rnd = new Random();
+
+            X += rnd.Next(-2, 2);
+            Y += rnd.Next(-2, 2);
+
+            return $"{Name} ({X},{Y})";
+        }
+    }
+}
---
+++ metods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace metods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person("Иванов", "Иван");
+            var person2 = new Person("Петров", "Петр");
+
+            for (int i = 0; i < 10; i++)
+            {
+                var position1 = person1.Run(1);
+                Console.WriteLine(position1);
+
+                var position2 = person2.Run(1);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(Factorial(10));
+            Console.ReadLine();
+
+
+            
+        }
+
+        public static int PrintHello(string name, int age)
+            {
+                Console.WriteLine($"Привет, {name}. Вам {age} лет");
+                return 1;
+            }
+
+        public static int Factorial(int value)
+        {
+            if (value <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return value * Factorial(value - 1);
+            }
+        }
+
+    }
+}
+    
